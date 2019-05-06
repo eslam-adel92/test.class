7bdf1313fa1f21fee3c1a103188a8c636c5b799a
eslam-adel92 <prog.eslamadel@gmail.com>
2019-05-06 22:47:12 +0200

    Fix all issue of the wordpress app and changing the mysql version to 5.7, also this commit containing the wp app only whilc commenting the code of the redmine app

--- destroy.sh
+++ destroy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 kubectl delete -f svc/
 kubectl delete -f configmaps/
-kubectl delete -f pvc/
-kubectl delete -f volumes/
 kubectl delete -f ingress.yml
 kubectl delete -f dbs/
 kubectl delete -f apps/
+kubectl delete -f pvc/
+kubectl delete -f volumes/
